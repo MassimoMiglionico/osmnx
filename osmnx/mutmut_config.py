fc0e9f5d92d0018b0b528198c9ad422d4f5e2f77
MassimoMiglionico <massimo.miglionico@gmail.com>
2023-06-11 19:31:25 +0200

    Derniers tests

--- osmnx/mutmut_config.py
+++ osmnx/mutmut_config.py
@@ -1,3 +1,3 @@
 def pre_mutation(context):
-    if context.filename not in 'geocoder.py':
+    if context.filename not in ['geocoder.py','folium.py','graph.py','downloader.py']:
         context.skip = True
